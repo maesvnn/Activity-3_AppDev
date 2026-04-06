7c068cd1a015358ecf5ad3b8d7b80c7f86d3a4b6
Bonna Mae Fajardo <bonnamae03@gmail.com>
2026-04-06 19:09:01 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+Console.WriteLine("===== YOLO REGISTRATION SYSTEM =====");
+
+Console.Write("Enter Full Name: ");
+string fullName = Console.ReadLine();
+
+Console.Write("Enter Age: ");
+int age = Convert.ToInt32(Console.ReadLine());
+
+if (age >= 18)
+{
+    Console.Write("Enter Username: ");
+    string username = Console.ReadLine();
+
+    Console.Write("Enter Password: ");
+    string password = Console.ReadLine();
+
+    Console.WriteLine("\n===== WELCOME =====");
+    Console.WriteLine("Name: " + fullName);
+    Console.WriteLine("Age: " + age);
+    Console.WriteLine("Username: " + username);
+    Console.WriteLine("Registered Successfully!");
+}
+else
+{
+    Console.WriteLine("\nRegistration Failed!");
+    Console.WriteLine("Oh no! You must be 18 years old and above to register.");
+}
+
+Console.WriteLine("\nPress any key to exit...");
+Console.ReadKey();
