ed8bc05ca8001636a95d92197b8139c1e4b7704d
Bonna Mae Fajardo <bonnamae03@gmail.com>
2026-04-06 19:20:43 +0800

    Revert "Updated welcome message in registration branch"
    This reverts commit 32a1a703cadfa2d5c1dd2ac47d6acc0fe22b4fe5.

--- Program.cs
+++ Program.cs
@@ -21,7 +21,6 @@ if (age >= 18)
     Console.WriteLine("Age: " + age);
     Console.WriteLine("Username: " + username);
     Console.WriteLine("Registered Successfully!");
-    Console.WriteLine("Welcome to YOLO Registration System!");
 }
 else
 {
